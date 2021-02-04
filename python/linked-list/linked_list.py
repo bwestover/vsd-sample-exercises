2362a9f17de36acf3f08bb92b8bd8b3c72d26b92
bwestover <bwestover@github.com>
2021-02-03 21:59:33 -0800

    add linked list in python

---
+++ python/linked-list/linked_list.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, value, next_node=None, prev_node=None):
+        pass
+
+
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def push(self, value):
+        # add value to the end of the list
+        pass
+
+    def pop(self):
+        # remove value from the end of the list (and return it)
+        pass
+
+    def shift(self):
+        # remove value from the beginning of the list (and return it)
+        pass
+
+    def unshift(self, value):
+        # add value to the beginning of the list
+        pass
+
+    def __len__(self):
+        # __len__ will enable the len() function for this object
+        pass
+
